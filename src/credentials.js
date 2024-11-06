1435d59e9de1613dd48dad0b59f4f877a6f6d372
october29 <fpuertas1992@proton.me>
2024-11-06 09:29:56 -0500

    fix: credentials

--- src/credentials.js
+++ src/credentials.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBn5HV5T1BBU4hrYmx6tQ9PyHSI-HFIckE",
-  authDomain: "chai-authenticate.firebaseapp.com",
-  projectId: "chai-authenticate",
-  storageBucket: "chai-authenticate.firebasestorage.app",
-  messagingSenderId: "113499989683",
-  appId: "1:113499989683:web:568bb7651842327860f2c6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
